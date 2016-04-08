663cca9d51d5e4e9ef3a1591aa45680ef718ade5
johannesvaltonen <johannes.valtonen@affecto.com>
2016-04-08 14:05:03 +0300

    Raised to release version.

--- Source/Mapping/Properties/AssemblyInfo.cs
+++ Source/Mapping/Properties/AssemblyInfo.cs
@@ -8,4 +8,4 @@
 [assembly: AssemblyFileVersion("2.1.0.0")]
 
 // This version is used by NuGet:
-[assembly: AssemblyInformationalVersion("2.1.0-prerelease01")]
+[assembly: AssemblyInformationalVersion("2.1.0")]
