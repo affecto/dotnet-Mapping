774a22de930a2891a7e2019fbd001a53cbd84208
Ville Henriksson <ville.henriksson@if.fi>
2017-06-22 11:14:06 +0300

    Removed MappingProfile Configure method and moved the code to the constructor instead.

--- Source/Mapping.AutoMapper/MappingProfile.cs
+++ Source/Mapping.AutoMapper/MappingProfile.cs
@@ -5,18 +5,18 @@ namespace Affecto.Mapping.AutoMapper
 {
     public abstract class MappingProfile<TSource, TDestination> : Profile, IMappingProfile<TSource, TDestination>
     {
+        protected MappingProfile()
+        {
+            IMappingExpression<TSource, TDestination> map = CreateMap<TSource, TDestination>();
+            ConfigureMapping(map);
+        }
+
         public virtual IMapper<TSource, TDestination> CreateMapper(IMapper mapper)
         {
             return new Mapper(mapper);
         }
 
         protected abstract void ConfigureMapping(IMappingExpression<TSource, TDestination> map);
-
-        protected override void Configure()
-        {
-            IMappingExpression<TSource, TDestination> map = CreateMap<TSource, TDestination>();
-            ConfigureMapping(map);
-        }
 
         public class Mapper : IMapper<TSource, TDestination>
         {
