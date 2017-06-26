324936506cb4396c5f4eb30c7f2d821684f887ec
Ville Henriksson <ville.henriksson@if.fi>
2017-06-26 11:42:37 +0300

    Removed TwoWayMappingProfile Configure method and moved the code to the constructor instead.

--- Source/Mapping.AutoMapper/Properties/AssemblyInfo.cs
+++ Source/Mapping.AutoMapper/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@
 [assembly: AssemblyDescription("AutoMapper implementation for one-way and two-way mapper interfaces defined in Affecto.Mapping NuGet.")]
 [assembly: AssemblyCompany("Affecto")]
 
-[assembly: AssemblyVersion("3.0.2.0")]
-[assembly: AssemblyFileVersion("3.0.2.0")]
+[assembly: AssemblyVersion("3.0.3.0")]
+[assembly: AssemblyFileVersion("3.0.3.0")]
 
 // This version is used by NuGet:
-[assembly: AssemblyInformationalVersion("3.0.2")]
+[assembly: AssemblyInformationalVersion("3.0.3")]
